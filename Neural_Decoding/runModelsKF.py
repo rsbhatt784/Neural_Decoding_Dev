1984351951c12e4cc5b91ad5ce42693f64a07b2d
rsbhatt784 <rbhatt1@uchicago.edu>
2022-06-08 15:26:28 -0500

    Added all 3 cross-bucket test.

--- Neural_Decoding/runModelsKF.py
+++ Neural_Decoding/runModelsKF.py
@@ -10,6 +10,7 @@ from Neural_Decoding.metrics import get_R2, get_rho, get_R2_parts
 def run_model_kf(input, output, training_range, testing_range, valid_range, type_of_R2):
    
     R2s = []
+    models = []
     for i in range(len(input)):
         curr_input = input[i]
         curr_output = output[i]
@@ -60,21 +61,287 @@ def run_model_kf(input, output, training_range, testing_range, valid_range, type
 
         #Fit model
         model.fit(X_train,y_train)
-
+        
+        #Save fitted models for later (i.e. cross-bucket tests)
+        models.append(model)
+    
         #Get predictions
         y_valid_predicted = model.predict(X_valid, y_valid)
 
         #Get metrics of fit (see read me for more details on the differences between metrics)
-        # 1st and 2nd entries that correspond to the positions
-        if type_of_R2 == "score":
+        # 1st and 2nd entries that correspond to the velocities
+        if type_of_R2 == "score": # Computing single-component FVAF
             R2_kf = get_R2(y_valid, y_valid_predicted)
-        elif type_of_R2 == "parts":
+        elif type_of_R2 == "parts": # Can be used to later compute combined FVAF
             R2_kf = get_R2_parts(y_valid, y_valid_predicted)
         R2s.append(R2_kf)
 
-            # #Next I'll get the rho^2 (the pearson correlation squared)
-            # rho_kf=get_rho(y_valid, y_valid_predicted_kf)
-            # #print('rho2:',rho_kf[0:2]**2) #I'm just printing the rho^2's of the 1st and 2nd entries that correspond to the positions
-            # Rhos.append(rho_kf) 
+        # if type_of_R2 == "parts" and type_of_test == "cross_bucket":
+        #     for c in range(len(input)):
+        #         cross_input = 
+        #         cross_output = 
+    return R2s, models 
+        
+        # if type_of_test == "cross_bucket":
+        #     models.append(model)
+        
+        # elif type_of_test == "within_bucket":
+        #     #Get predictions
+        #     y_valid_predicted = model.predict(X_valid, y_valid)
 
-    return R2s
+        #     #Get metrics of fit (see read me for more details on the differences between metrics)
+        #     # 1st and 2nd entries that correspond to the velocities
+        #     if type_of_R2 == "score": # Computing single-component FVAF
+        #         R2_kf = get_R2(y_valid, y_valid_predicted)
+        #     elif type_of_R2 == "parts": # Can be used to later compute combined FVAF
+        #         R2_kf = get_R2_parts(y_valid, y_valid_predicted)
+        #     R2s.append(R2_kf)
+        
+
+
+def cross_bucket_test(models, input, output, training_range, testing_range, valid_range, type_of_R2):
+ 
+    """
+    (SIKE) Just need X_valid and y_valid from the cross-bucket in order to predict its kinematics using model trained on the other bucket.
+    """
+    
+    XY_FVAF = [[] for i in range(len(input))]
+    Nomen = [[] for i in range(len(input))]
+    Denom = [[] for i in range(len(input))]
+    total_residual = []
+    
+    for m in range(len(models)): # but in reality, nModels should/will always be the same as nCrossBuckets
+        R2s = []
+        for c in range(len(input)):
+            if m == c: 
+                 continue 
+            else: 
+                curr_input = input[c]
+                curr_output = output[c]
+                num_examples=curr_input.shape[0] # nRows (b/c nCols = number of units)
+                    
+                #Note that each range has a buffer of 1 bin at the beginning and end
+                #This makes it so that the different sets don't include overlapping data
+                training_set=np.arange(int(np.round(training_range[0]*num_examples))+1,int(np.round(training_range[1]*num_examples))-1)
+                testing_set=np.arange(int(np.round(testing_range[0]*num_examples))+1,int(np.round(testing_range[1]*num_examples))-1)
+                valid_set=np.arange(int(np.round(valid_range[0]*num_examples))+1,int(np.round(valid_range[1]*num_examples))-1)  
+
+                #Get training data
+                X_train=curr_input[training_set,:]
+                y_train=curr_output[training_set,:]
+
+                #Get testing data
+                X_test=curr_input[testing_set,:]
+                y_test=curr_output[testing_set,:]
+
+                #Get validation data
+                X_valid=curr_input[valid_set,:]
+                y_valid=curr_output[valid_set,:]  
+
+                #Z-score "X" inputs. 
+                X_train_mean=np.nanmean(X_train,axis=0)
+                X_train_std=np.nanstd(X_train,axis=0)
+                X_train=(X_train-X_train_mean)/X_train_std
+                X_test=(X_test-X_train_mean)/X_train_std
+                X_valid=(X_valid-X_train_mean)/X_train_std
+
+                #Zero-center outputs
+                y_train_mean=np.mean(y_train,axis=0)
+                y_train=y_train-y_train_mean
+                y_test=y_test-y_train_mean
+                y_valid=y_valid-y_train_mean
+                
+                #Get predictions
+                y_valid_predicted = models[m].predict(X_valid, y_valid)
+
+                #Get metrics of fit (see read me for more details on the differences between metrics)
+                # 1st and 2nd entries that correspond to the velocities
+                if type_of_R2 == "score": # Computing single-component FVAF
+                    R2_kf = get_R2(y_valid, y_valid_predicted)
+                elif type_of_R2 == "parts": # Can be used to later compute combined FVAF
+                    R2_kf = get_R2_parts(y_valid, y_valid_predicted)
+                    # print(R2_kf)
+                    # print(R2_kf.shape)
+                R2s.append(R2_kf)
+
+                # Compute combined XY_FVAF
+                vel_x_nom = R2_kf[0][0] # dim = (nom, x_vel)
+                vel_x_denom = R2_kf[1][0] # dim = (denom, x_vel)
+                vel_y_nom = R2_kf[0][1] # dim = (nom, y_vel)
+                vel_y_denom = R2_kf[1][1] # dim = (denom, y_vel)
+                nom = vel_x_nom + vel_y_nom
+                denom = vel_x_denom + vel_y_denom
+
+                combined_FVAF = 1 - (nom / denom)
+                XY_FVAF[m].append(combined_FVAF)
+
+                Nomen[m].append(nom)
+                Denom[m].append(denom)
+
+        total_residual.append(1 - (sum(Nomen[m]) / sum(Denom[m])))
+        #totalResidual(m,1) = 1 - (sum(Nom(m,:)) / (sum(xDen(m,:)) + sum(yDen(m,:))) );         
+
+                # % Compute combined FVAF 
+                # Nomen = ((actualX - predX).^2) + ((actualY - predY).^2);
+                # xDenom = sum((actualX - mean(actualX)).^2);
+                # yDenom = sum((actualY - mean(actualY)).^2);
+                # combinedFVAF(m,c) = 1 - (sum(Nomen)/(xDenom + yDenom));
+                
+                # xDen(m,c) = xDenom;
+                # yDen(m,c) = yDenom;
+                # Nom(m,c) = sum(Nomen);
+
+    return XY_FVAF, total_residual
+
+
+def cross_polarity_test(models, input, output, training_range, testing_range, valid_range, type_of_R2):   
+    """
+    (SIKE) Just need X_valid and y_valid from the cross-bucket in order to predict its kinematics using model trained on the other bucket.
+    """
+    
+    XY_FVAF = []
+    for m in range(len(models)): # but in reality, nModels should/will always be the same as nCrossBuckets
+       
+        # n is the index of the bucket with opposite polarity 
+        if m <= 7:
+            n = m + 8
+        else:
+            n = m - 8
+
+        curr_input = input[n]
+        curr_output = output[n]
+        num_examples=curr_input.shape[0] # nRows (b/c nCols = number of units)
+                
+        #Note that each range has a buffer of 1 bin at the beginning and end
+        #This makes it so that the different sets don't include overlapping data
+        training_set=np.arange(int(np.round(training_range[0]*num_examples))+1,int(np.round(training_range[1]*num_examples))-1)
+        testing_set=np.arange(int(np.round(testing_range[0]*num_examples))+1,int(np.round(testing_range[1]*num_examples))-1)
+        valid_set=np.arange(int(np.round(valid_range[0]*num_examples))+1,int(np.round(valid_range[1]*num_examples))-1)  
+
+        #Get training data
+        X_train=curr_input[training_set,:]
+        y_train=curr_output[training_set,:]
+
+        #Get testing data
+        X_test=curr_input[testing_set,:]
+        y_test=curr_output[testing_set,:]
+
+        #Get validation data
+        X_valid=curr_input[valid_set,:]
+        y_valid=curr_output[valid_set,:]  
+
+        #Z-score "X" inputs. 
+        X_train_mean=np.nanmean(X_train,axis=0)
+        X_train_std=np.nanstd(X_train,axis=0)
+        X_train=(X_train-X_train_mean)/X_train_std
+        X_test=(X_test-X_train_mean)/X_train_std
+        X_valid=(X_valid-X_train_mean)/X_train_std
+
+        #Zero-center outputs
+        y_train_mean=np.mean(y_train,axis=0)
+        y_train=y_train-y_train_mean
+        y_test=y_test-y_train_mean
+        y_valid=y_valid-y_train_mean
+        
+        #Get predictions
+        y_valid_predicted = models[m].predict(X_valid, y_valid)
+
+        #Get metrics of fit (see read me for more details on the differences between metrics)
+        # 1st and 2nd entries that correspond to the velocities
+        if type_of_R2 == "score": # Computing single-component FVAF
+            R2_kf = get_R2(y_valid, y_valid_predicted)
+        elif type_of_R2 == "parts": # Can be used to later compute combined FVAF
+            R2_kf = get_R2_parts(y_valid, y_valid_predicted)
+
+        # Compute combined XY_FVAF
+        vel_x_nom = R2_kf[0][0] # dim = (nom, x_vel)
+        vel_x_denom = R2_kf[1][0] # dim = (denom, x_vel)
+        vel_y_nom = R2_kf[0][1] # dim = (nom, y_vel)
+        vel_y_denom = R2_kf[1][1] # dim = (denom, y_vel)
+        nom = vel_x_nom + vel_y_nom
+        denom = vel_x_denom + vel_y_denom
+
+        combined_FVAF = 1 - (nom / denom)
+        XY_FVAF.append(combined_FVAF)
+
+    return XY_FVAF
+
+
+
+def complete_opposite_bucket_test(models, input, output, training_range, testing_range, valid_range, type_of_R2):   
+    """
+    (SIKE) Just need X_valid and y_valid from the cross-bucket in order to predict its kinematics using model trained on the other bucket.
+    """
+    
+    XY_FVAF = []
+    for m in range(len(models)): # but in reality, nModels should/will always be the same as nCrossBuckets
+        
+        # n is the index of the bucket with opposite direction and polarity 
+        # (based on the 'combos' variable which specifies bucket labels)
+        if m >= 0 and m <= 3:    
+            n = m + 12
+        elif m >= 4 and m <= 7:
+            n = m + 4
+        elif m >= 8 and m <= 11:
+            n = m - 4
+        elif m >= 12 and m <= 15:
+            n = m - 12
+
+        curr_input = input[n]
+        curr_output = output[n]
+        num_examples=curr_input.shape[0] # nRows (b/c nCols = number of units)
+                
+        #Note that each range has a buffer of 1 bin at the beginning and end
+        #This makes it so that the different sets don't include overlapping data
+        training_set=np.arange(int(np.round(training_range[0]*num_examples))+1,int(np.round(training_range[1]*num_examples))-1)
+        testing_set=np.arange(int(np.round(testing_range[0]*num_examples))+1,int(np.round(testing_range[1]*num_examples))-1)
+        valid_set=np.arange(int(np.round(valid_range[0]*num_examples))+1,int(np.round(valid_range[1]*num_examples))-1)  
+
+        #Get training data
+        X_train=curr_input[training_set,:]
+        y_train=curr_output[training_set,:]
+
+        #Get testing data
+        X_test=curr_input[testing_set,:]
+        y_test=curr_output[testing_set,:]
+
+        #Get validation data
+        X_valid=curr_input[valid_set,:]
+        y_valid=curr_output[valid_set,:]  
+
+        #Z-score "X" inputs. 
+        X_train_mean=np.nanmean(X_train,axis=0)
+        X_train_std=np.nanstd(X_train,axis=0)
+        X_train=(X_train-X_train_mean)/X_train_std
+        X_test=(X_test-X_train_mean)/X_train_std
+        X_valid=(X_valid-X_train_mean)/X_train_std
+
+        #Zero-center outputs
+        y_train_mean=np.mean(y_train,axis=0)
+        y_train=y_train-y_train_mean
+        y_test=y_test-y_train_mean
+        y_valid=y_valid-y_train_mean
+        
+        #Get predictions
+        y_valid_predicted = models[m].predict(X_valid, y_valid)
+
+        #Get metrics of fit (see read me for more details on the differences between metrics)
+        # 1st and 2nd entries that correspond to the velocities
+        if type_of_R2 == "score": # Computing single-component FVAF
+            R2_kf = get_R2(y_valid, y_valid_predicted)
+        elif type_of_R2 == "parts": # Can be used to later compute combined FVAF
+            R2_kf = get_R2_parts(y_valid, y_valid_predicted)
+
+        # Compute combined XY_FVAF
+        vel_x_nom = R2_kf[0][0] # dim = (nom, x_vel)
+        vel_x_denom = R2_kf[1][0] # dim = (denom, x_vel)
+        vel_y_nom = R2_kf[0][1] # dim = (nom, y_vel)
+        vel_y_denom = R2_kf[1][1] # dim = (denom, y_vel)
+        nom = vel_x_nom + vel_y_nom
+        denom = vel_x_denom + vel_y_denom
+
+        combined_FVAF = 1 - (nom / denom)
+        XY_FVAF.append(combined_FVAF)
+
+    return XY_FVAF
+            
